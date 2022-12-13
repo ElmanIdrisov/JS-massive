6bacdc7f7783cd0aad0a5d58b619e0c3ba313de2
ElmanIdrisov <elmanidrisov222@gmail.com>
2022-12-13 13:06:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+ const arr = ['milk', 'egg', 'apple', 'oil','potato','banana','watermelon'];
+/*  const arrOfNum = [10, 20, 30, 40, 50, 60, 70, 80]; */
+
+/* let selectedProduct = null;
+const searchProduct = 'banana'
+
+
+
+
+for (let index = 0; index < arr.length; index++) {
+    
+    const product = arr[index];
+    if(product === searchProduct){
+        selectedProduct = product;
+    }
+}
+
+
+console.log('selectedProduct:', selectedProduct); */ 
+
+/* const changedArr = arrOfNum.map((number) => {
+    return number * 2;
+})
+
+console.log('arr', arr);
+console.log('changedArr', changedArr); */
+
+/* const foundProduct = arr.find((product) => {
+    console.log('product: ', product);
+    return product ===  'oil'
+})
+
+console.log('foundProduct: ', foundProduct);
+
+ */
+ 
