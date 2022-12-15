63a08c4fc8b02e81252b7f9a957ae2e354271316
ElmanIdrisov <elmanidrisov222@gmail.com>
2022-12-15 13:50:42 +0600

    js add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const arr = ['milk', 'egg', 'apple', 'oil','potato','banana','watermelon'];
+ /* const arr = ['milk', 'egg', 'apple', 'oil','potato','banana','watermelon']; */
 /*  const arrOfNum = [10, 20, 30, 40, 50, 60, 70, 80]; */
 
 /* let selectedProduct = null;
@@ -33,4 +33,43 @@ console.log('changedArr', changedArr); */
 console.log('foundProduct: ', foundProduct);
 
  */
+
+/* const arr = ['dog', 'cat', 'mouse', 'lion', 'tiger'];
+
+let findAnimal  
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    if (element === 'cat') {
+        findAnimal = element;
+    }
+}
+
+console.log('findAnimal: ', findAnimal); */
  
+const arr = [
+    {
+        name: 'Denis',
+        hobbie: 'swimming'
+    },
+    {
+        name: 'Elman',
+        hobbie: 'drawing'
+    },
+    {
+        name: 'Ahmed',
+        hobbie: 'singing'
+    },
+    {
+        name: 'Adil',
+        hobbie: 'collecting'
+    },
+    {
+        name: 'Isa',
+        hobbie: 'swimming'
+    }
+
+]
+
+const filteredArray = arr.filter((element) => element.hobbie === 'swimming');
+console.log('filteredArray: ', filteredArray);
